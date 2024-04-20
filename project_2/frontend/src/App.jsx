fbb4b4c78468163f11fa45c0cf0b6e0cf22a1c01
Ashfaq684 <ashfaq.mostahid@gmail.com>
2024-04-20 21:11:25 +0600

    django-react project

---
+++ project_2/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import TodoForm from './components/TodoForm'
+import table from './components/table'
+
+function App() {
+
+  return (
+    <div className='bg-indigo-100 px-8 min-h-screen'>
+      <nav className='pt-8'>
+        <h1 className='text-5xl text-center'>ToDo List</h1>
+      </nav>
+    </div>
+  )
+}
+
+export default App
