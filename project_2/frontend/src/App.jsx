5e08af13ef49b7bf589ba3706f3e980ccbeaba94
Ashfaq684 <ashfaq.mostahid@gmail.com>
2024-04-21 23:18:38 +0600

    django-react project

--- project_2/frontend/src/App.jsx
+++ project_2/frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import TodoForm from './components/TodoForm'
-import table from './components/table'
+import Table from './components/Table'
 
 function App() {
 
   return (
     <div className='bg-indigo-100 px-8 min-h-screen'>
       <nav className='pt-8'>
-        <h1 className='text-5xl text-center'>ToDo List</h1>
+        <h1 className='text-5xl text-center pb-12'>ToDo List</h1>
       </nav>
+      <TodoForm />
+      <Table />
     </div>
   )
 }
