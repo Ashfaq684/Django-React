80b4cb8d03a40c0f53a50b70b03dc615506e18a1
Ashfaq684 <ashfaq.mostahid@gmail.com>
2024-04-23 12:51:00 +0600

    django-react project

--- project_2/frontend/src/components/table.jsx
+++ project_2/frontend/src/components/table.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import { MdOutlineDeleteOutline, MdEditNote, MdOutlineCheckBox, MdOutlineCheckBoxOutlineBlank } from 'react-icons/md'
 
 function Table({todos, setTodos, isLoading}) {
+
+  const [editText, setEditText] = useState({
+    'body': ''
+  })
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://127.0.0.1:8000/api/todo/${id}/`)
+      const newList = todos.filter(todo => todo.id !== id)
+      setTodos(newList)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleEdit = async (id, value) => {
+    try {
+      const response = await axios.patch(`http://127.0.0.1:8000/api/todo/${id}/`, value)
+      const newTodos = todos.map(todo => todo.id === id ? response.data : todo)
+      setTodos(newTodos)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleCheckbox = (id, value) => {
+    handleEdit(id, {
+      'completed': !value
+    })
+  }
+
+  const handleChange = (e) => {
+    setEditText(prev => ({
+      ...prev,
+      'body': e.target.value
+    }))
+  }
+
+  const handleClick = () => {
+    handleEdit(editText.id, editText)
+    setEditText({ 
+      'body': '' 
+    })
+  }
+
   return (
     <div className='py-2'>
       <table className='w-11/12 max-w-4xl'>
@@ -17,20 +63,25 @@ function Table({todos, setTodos, isLoading}) {
         <tbody>
           {isLoading ? <div>Is Loading</div> :
             <>
-              {todos.map((todItem, index) => {
+              {todos.map((todoItem, index) => {
                 return (
-                  <tr key={todItem.id} className='border-b border-black'>
-                    <td className='p-3'>
-                      <span className='inline-block cursor-pointer'><MdOutlineCheckBox /></span>
+                  <tr key={todoItem.id} className='border-b border-black'>
+                    <td className='p-3' title={todoItem.id}>
+                      <span onClick={() => handleCheckbox(todoItem.id, todoItem.completed)}
+                        className='inline-block cursor-pointer'>{todoItem.completed ?<MdOutlineCheckBox /> : <MdOutlineCheckBoxOutlineBlank /> }</span>
                     </td>
-                    <td className='p-3 text-sm'>{todItem.body}</td>
-                    <td className='p-3 text-sm'>
-                      <span className='p-1.5 text-xs font-medium tracking-wider rounded-md bg-green-300'>done</span>
+                    <td className='p-3 text-sm'>{todoItem.body}</td>
+                    <td className='p-3 text-sm text-center'>
+                      <span className={`p-1.5 text-xs font-medium tracking-wider rounded-md ${todoItem.completed ? 'bg-green-300' : 'bg-red-300'}`}>{todoItem.completed ? 'Done' : 'Incomplete'}</span>
                     </td>
-                    <td className='p-3 text-sm'>22-04-24</td>
+                    <td className='p-3 text-sm'>{new Date(todoItem.created_at).toLocaleString()}</td>
                     <td className='p-3 text-sm font-medium grid grid-flow-col items-center mt-5'>
-                      <span className='text-xl cursor-pointer'><MdEditNote /></span>
-                      <span className='text-xl cursor-pointer'><MdOutlineDeleteOutline /></span>
+                      <span className='text-xl cursor-pointer'>
+                        <label htmlFor="my_modal_6" className="btn">
+                          <MdEditNote onClick={() => setEditText(todoItem)} />
+                        </label>
+                      </span>
+                      <span className='text-xl cursor-pointer'><MdOutlineDeleteOutline onClick={() => handleDelete(todoItem.id)} /></span>
                     </td>
                   </tr>
                 )
@@ -40,6 +91,20 @@ function Table({todos, setTodos, isLoading}) {
           }
         </tbody>
       </table>
+
+      {/* Put this part before </body> tag */}
+      <input type="checkbox" id="my_modal_6" className="modal-toggle" />
+      <div className="modal" role="dialog">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Edit Todo</h3>
+          <input type="text" value={editText.body} onChange={handleChange} className="input input-bordered w-full mt-8" />
+          <div className="modal-action">
+            <label htmlFor="my_modal_6" onClick={handleClick} className="btn btn-primary">Edit</label>
+            <label htmlFor="my_modal_6" className="btn">Close</label>
+          </div>
+        </div>
+      </div>
+
     </div>
     
   )
